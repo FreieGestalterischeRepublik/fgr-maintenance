6bc1439ade754393763056fa8014b87978f8242c
Freie Gestalterische Republik <marc@fgr.design>
2026-07-13 15:44:58 +0200

    chore: fgr-email-encoder und fgr-duplicate-post in mu-sync-Liste aufgenommen

--- fgr-maintenance.php
+++ fgr-maintenance.php
@@ -104,6 +104,8 @@ add_action( 'upgrader_process_complete', function ( $upgrader, array $hook_extra
         'fgr-mail-smtp/fgr-mail-smtp.php',
         'fgr-hide-login/fgr-hide-login.php',
         'fgr-maintenance/fgr-maintenance.php',
+        'fgr-email-encoder/fgr-email-encoder.php',
+        'fgr-duplicate-post/fgr-duplicate-post.php',
     ];
 
     $updated = array_merge(
